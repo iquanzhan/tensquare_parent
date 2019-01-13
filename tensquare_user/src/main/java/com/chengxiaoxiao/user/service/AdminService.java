31bbed88731664d0ae7712fbf6f1a6401237e0f2
iquanzhan <iquanzhan@163.com>
2019-01-13 15:55:04 +0800

    删除管理员时，验证权限

--- tensquare_user/src/main/java/com/chengxiaoxiao/user/service/AdminService.java
+++ tensquare_user/src/main/java/com/chengxiaoxiao/user/service/AdminService.java
@@ -18,138 +18,162 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import javax.servlet.http.HttpServletRequest;
 
+import io.jsonwebtoken.Claims;
 import util.IdWorker;
+import util.JwtUtil;
 
 /**
  * 服务层
- * 
  * @author Administrator
- *
  */
 @Service
 public class AdminService {
 
-	@Autowired
-	private AdminDao adminDao;
-	
-	@Autowired
-	private IdWorker idWorker;
+    @Autowired
+    private AdminDao adminDao;
 
-	@Autowired
-	private BCryptPasswordEncoder encoder;
+    @Autowired
+    private IdWorker idWorker;
 
+    @Autowired
+    private BCryptPasswordEncoder encoder;
 
-	/**
-	 * 查询全部列表
-	 * @return
-	 */
-	public List<Admin> findAll() {
-		return adminDao.findAll();
-	}
+    @Autowired
+    private HttpServletRequest request;
 
-	
-	/**
-	 * 条件查询+分页
-	 * @param whereMap
-	 * @param page
-	 * @param size
-	 * @return
-	 */
-	public Page<Admin> findSearch(Map whereMap, int page, int size) {
-		Specification<Admin> specification = createSpecification(whereMap);
-		PageRequest pageRequest =  PageRequest.of(page-1, size);
-		return adminDao.findAll(specification, pageRequest);
-	}
+    @Autowired
+    private JwtUtil jwtUtil;
 
-	/**
-	 * 条件查询
-	 * @param whereMap
-	 * @return
-	 */
-	public List<Admin> findSearch(Map whereMap) {
-		Specification<Admin> specification = createSpecification(whereMap);
-		return adminDao.findAll(specification);
-	}
+    /**
+     * 查询全部列表
+     * @return
+     */
+    public List<Admin> findAll() {
+        return adminDao.findAll();
+    }
 
-	/**
-	 * 根据ID查询实体
-	 * @param id
-	 * @return
-	 */
-	public Admin findById(String id) {
-		return adminDao.findById(id).get();
-	}
+    /**
+     * 条件查询+分页
+     * @param whereMap
+     * @param page
+     * @param size
+     * @return
+     */
+    public Page<Admin> findSearch(Map whereMap, int page, int size) {
+        Specification<Admin> specification = createSpecification(whereMap);
+        PageRequest pageRequest = PageRequest.of(page - 1, size);
+        return adminDao.findAll(specification, pageRequest);
+    }
 
-	/**
-	 * 增加
-	 * @param admin
-	 */
-	public void add(Admin admin) {
-		admin.setId( idWorker.nextId()+"" );
-		admin.setPassword(encoder.encode(admin.getPassword()));
-		adminDao.save(admin);
-	}
+    /**
+     * 条件查询
+     * @param whereMap
+     * @return
+     */
+    public List<Admin> findSearch(Map whereMap) {
+        Specification<Admin> specification = createSpecification(whereMap);
+        return adminDao.findAll(specification);
+    }
 
-	/**
-	 * 修改
-	 * @param admin
-	 */
-	public void update(Admin admin) {
-		adminDao.save(admin);
-	}
+    /**
+     * 根据ID查询实体
+     * @param id
+     * @return
+     */
+    public Admin findById(String id) {
+        return adminDao.findById(id).get();
+    }
 
-	/**
-	 * 删除
-	 * @param id
-	 */
-	public void deleteById(String id) {
-		adminDao.deleteById(id);
-	}
+    /**
+     * 增加
+     * @param admin
+     */
+    public void add(Admin admin) {
+        admin.setId(idWorker.nextId() + "");
+        admin.setPassword(encoder.encode(admin.getPassword()));
+        adminDao.save(admin);
+    }
 
-	/**
-	 * 动态条件构建
-	 * @param searchMap
-	 * @return
-	 */
-	private Specification<Admin> createSpecification(Map searchMap) {
+    /**
+     * 修改
+     * @param admin
+     */
+    public void update(Admin admin) {
+        adminDao.save(admin);
+    }
 
-		return new Specification<Admin>() {
+    /**
+     * 删除
+     * @param id
+     */
+    public void deleteById(String id) {
+        String authorization = request.getHeader("Authorization");
+        if (authorization == null || "".equals(authorization)) {
+            throw new RuntimeException("权限不足");
+        }
+        if (!authorization.startsWith("Bearer ")) {
+            throw new RuntimeException("权限不足");
+        }
+        String token = authorization.substring(7);
 
-			@Override
-			public Predicate toPredicate(Root<Admin> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-				List<Predicate> predicateList = new ArrayList<Predicate>();
+        try {
+            Claims claims = jwtUtil.parseJWT(token);
+            String role = (String) claims.get("roles");
+            if (role == null || "".equals(role) || !"admin".equals(role)) {
+                throw new RuntimeException("权限不足");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("权限不足");
+        }
+
+        adminDao.deleteById(id);
+    }
+
+    /**
+     * 动态条件构建
+     * @param searchMap
+     * @return
+     */
+    private Specification<Admin> createSpecification(Map searchMap) {
+
+        return new Specification<Admin>() {
+
+            @Override
+            public Predicate toPredicate(Root<Admin> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                List<Predicate> predicateList = new ArrayList<Predicate>();
                 // ID
-                if (searchMap.get("id")!=null && !"".equals(searchMap.get("id"))) {
-                	predicateList.add(cb.like(root.get("id").as(String.class), "%"+(String)searchMap.get("id")+"%"));
+                if (searchMap.get("id") != null && !"".equals(searchMap.get("id"))) {
+                    predicateList.add(cb.like(root.get("id").as(String.class), "%" + (String) searchMap.get("id") + "%"));
                 }
                 // 登陆名称
-                if (searchMap.get("loginname")!=null && !"".equals(searchMap.get("loginname"))) {
-                	predicateList.add(cb.like(root.get("loginname").as(String.class), "%"+(String)searchMap.get("loginname")+"%"));
+                if (searchMap.get("loginname") != null && !"".equals(searchMap.get("loginname"))) {
+                    predicateList.add(cb.like(root.get("loginname").as(String.class), "%" + (String) searchMap.get("loginname") + "%"));
                 }
                 // 密码
-                if (searchMap.get("password")!=null && !"".equals(searchMap.get("password"))) {
-                	predicateList.add(cb.like(root.get("password").as(String.class), "%"+(String)searchMap.get("password")+"%"));
+                if (searchMap.get("password") != null && !"".equals(searchMap.get("password"))) {
+                    predicateList.add(cb.like(root.get("password").as(String.class), "%" + (String) searchMap.get("password") + "%"));
                 }
                 // 状态
-                if (searchMap.get("state")!=null && !"".equals(searchMap.get("state"))) {
-                	predicateList.add(cb.like(root.get("state").as(String.class), "%"+(String)searchMap.get("state")+"%"));
+                if (searchMap.get("state") != null && !"".equals(searchMap.get("state"))) {
+                    predicateList.add(cb.like(root.get("state").as(String.class), "%" + (String) searchMap.get("state") + "%"));
                 }
-				
-				return cb.and( predicateList.toArray(new Predicate[predicateList.size()]));
 
-			}
-		};
+                return cb.and(predicateList.toArray(new Predicate[predicateList.size()]));
 
-	}
+            }
+        };
 
-	public Admin login(Admin admin) {
+    }
 
-		Admin adminDB = adminDao.findByLoginname(admin.getLoginname());
+    public Admin login(Admin admin) {
 
-		if(adminDB!=null&&encoder.matches(admin.getPassword(),adminDB.getPassword())){
-			return adminDB;
-		}
-		return null;
-	}
+        Admin adminDB = adminDao.findByLoginname(admin.getLoginname());
+
+        if (adminDB != null && encoder.matches(admin.getPassword(), adminDB.getPassword())) {
+            return adminDB;
+        }
+        return null;
+    }
 }
